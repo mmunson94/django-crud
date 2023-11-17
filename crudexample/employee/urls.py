f712163c768d9baf68c70f39abaad3207d2a8cd9
mmunson94 <michaeljackmunson9994@outlook.com>
2023-11-17 18:09:26 +0000

    Update functionality works

--- crudexample/employee/urls.py
+++ crudexample/employee/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('list/', views.employee_list, name='employee_list'),
     path('add/', views.employee_add_and_update, name='employee_add'),
-    # path('update/<int:id>', views.employee_add_and_update, name='employee_update')
+    path('update/<int:id>', views.employee_add_and_update, name='employee_update')
 ]
