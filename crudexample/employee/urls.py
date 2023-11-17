dca5e740e07ae419ffcd185c9c85956c63989203
mmunson94 <michaeljackmunson9994@outlook.com>
2023-11-17 18:03:47 +0000

    Modified add so update can be added

--- crudexample/employee/urls.py
+++ crudexample/employee/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('list/', views.employee_list, name='employee_list'),
-    path('add/', views.employee_add_and_update, name='employee_add_and_update'),
+    path('add/', views.employee_add_and_update, name='employee_add'),
+    # path('update/<int:id>', views.employee_add_and_update, name='employee_update')
 ]
